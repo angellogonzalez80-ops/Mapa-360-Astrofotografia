917f177fbd986549f44840d75dac8c81a388b30a
angellogonzalez80-ops <angellogonzalez80@gmail.com>
2026-03-17 13:16:49 -0300

    Create puntos.js

---
+++ puntos.js
@@ -0,0 +1,19 @@
+// Aquí irás agregando tus expediciones
+const MIS_PUNTOS = [
+    {
+        lat: -32.729, 
+        lng: -70.551, 
+        nombre: "Torreon Astronomico - San Esteban",
+        bortle: "Clase 3",
+        altitud: "1.200 msnm",
+        foto360: "https://tour.panoee.net/69b97847e1475b55ee82c630" // Cambia esto por tu link
+    },
+    {
+        lat: -39.385, 
+        lng: -71.940, 
+        nombre: "Villarrica - Volcán Nocturno",
+        bortle: "Clase 3",
+        altitud: "1.400 msnm",
+        foto360: "TU_LINK_DE_PANOEE_AQUÍ"
+    }
+];
